607f7a2f6e5afa4e06884e34f36ccc99517e6215
David <david@outlier.org>
2019-06-14 12:26:08 -0700

    init

---
+++ test.js
@@ -0,0 +1,21 @@
+const tape = require('tape')
+const jsonist = require('jsonist')
+
+const PORT = process.env.PORT = process.env.PORT || require('get-PORT-sync')()
+const server = require('./server')
+
+const urlBase = `http://localhost:${PORT}`
+
+tape('should respond hello', (t) => {
+  jsonist.get(urlBase, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.msg, 'hello')
+    t.end()
+  })
+})
+
+tape('cleanup', function (t) {
+  server.close()
+  t.end()
+})
