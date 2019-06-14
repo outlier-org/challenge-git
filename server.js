f1ded694e001e7d5c382a47c0945c8df5f23a5f6
David <david@outlier.org>
2019-06-14 13:26:16 -0700

    feat: add user-agent endpoint

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
+  if (req.url === '/user-agent') return respondUserAgent(req, res)
 
   res.end()
 })
 
 function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
+}
+
+function respondUserAgent (req, res) {
+  const ua = req.headers['user-agent']
+  res.end(JSON.stringify({ ua }))
 }
 
 server.listen(PORT)
